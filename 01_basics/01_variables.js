e9da6ae0c13f27f2d90cce0d56901aa7fa462299
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-04 18:28:22 +0000

    #variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "atulkumar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+//accountId = 2345
+
+accountEmail = "@accountEmail.com"
+accountPassword = "12345678"
+accountCity= "delhi"
+console.log(accountId);
+console.table([accountId, accountEmail, accountId, accountPassword])
