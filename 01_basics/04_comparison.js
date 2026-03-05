a9c6fe6b248c9ebc9b698c0d61367b343cf5acc6
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-05 06:47:07 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedIn = false;
+const OutsideTemp = null
+let userEmail 
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id=== anotherId);
