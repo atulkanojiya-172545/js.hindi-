76d4475cc5dd518559495fde86070174c18a82bf
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-06 10:41:57 +0000

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,8 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
