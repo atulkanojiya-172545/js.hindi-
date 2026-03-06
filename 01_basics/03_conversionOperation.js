f358ac815f68334eae21b53dedc91574147d35c3
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-06 11:27:08 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,11 @@
 let score = 33
 
-console.log(typeof(score ));
+//console.log(typeof(score ));
+// operations 
+let value  = 3
+let negValue = -value
+//console.log(negvalue );
+let str1  ="Hello"
+let str2 = " Atul"
+let str3 = str1+str2
+console.log(str3);
