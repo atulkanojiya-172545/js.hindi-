a2cf5ca4fc5610f5052d7365cb28731ed29a5443
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-06 11:26:18 +0000

    objects

---
+++ 01_basics/03_objects.js
@@ -0,0 +1,12 @@
+//object literals 
+const JsUser = {
+    name :"Atul",
+    age: 20,
+    location: "Lucknow",
+    email : "atulak@gmail.com",
+    isLoggedin : false
+}
+// accessing objects
+
+console.log(JsUser.email)
+console.log(JsUser["email"])
