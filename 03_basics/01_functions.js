c886a2874d89362b0d12b8eb3920ccfaf0c78979
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-07 12:46:12 +0000

    functionns js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+ const user = {
+    username : "Hitesh",
+    price : 299
+ }
+ function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price }`);
+    
+ }
+ handleObject(user)
