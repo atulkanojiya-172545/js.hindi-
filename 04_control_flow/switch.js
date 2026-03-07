234df2578a179df83634d143f7f8950a5e00639f
atulkanojiya-172545 <atulkanojiya.jnv@gmail.com>
2026-03-07 17:36:11 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("Janauary");
+        break;
+    case 2:
+        console.log("Febrauary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+
+    default:
+        console.log("November");
+        
+        break;
+}
